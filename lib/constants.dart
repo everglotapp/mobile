0b8d3e77ca115cc4226e89fb701cc0d34ef0da23
haveyaseen <haveyaread@gmail.com>
2021-06-13 16:21:35 +0200

    use web client ID for google sign in on android

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,8 @@
-const GOOGLE_CLIENT_ID =
+const GOOGLE_CLIENT_ID_IOS =
     "457984069949-79sdutia34vvkn2fcetcq1sblmhe38gk.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID_ANDROID =
+    "457984069949-5giaecr690rps0g9f5unj0j5j1qt22ck.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID_WEB =
+    "457984069949-bgc3aj14fi47olkp0arn7is4cr07cfla.apps.googleusercontent.com";
 
 const EVERGLOT_URL = 'https://app.everglot.com';
--- lib/login.dart
+++ lib/login.dart
@@ -1,8 +1,24 @@
+import 'dart:io';
+
 import 'package:everglot/constants.dart';
 import 'package:everglot/webapp.dart';
 import 'package:flutter/material.dart';
 import 'package:auth_buttons/auth_buttons.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
+String _getGoogleClientId() {
+  // Causes Platform exception 10
+  // if (Platform.isAndroid) {
+  //   print("Using Android client ID");
+  //   return GOOGLE_CLIENT_ID_ANDROID;
+  // }
+  if (Platform.isIOS) {
+    print("Using iOS client ID");
+    return GOOGLE_CLIENT_ID_IOS;
+  }
+  print("Using web client ID");
+  return GOOGLE_CLIENT_ID_WEB;
+}
 
 class LoginPageArguments {
   bool signedOut = false;
@@ -18,7 +34,7 @@ class LoginPage extends StatefulWidget {
 class LoginPageState extends State<LoginPage> {
   // GoogleSignInAccount? _currentUser;
   GoogleSignIn _googleSignIn = GoogleSignIn(
-    clientId: GOOGLE_CLIENT_ID,
+    clientId: _getGoogleClientId(),
     scopes: <String>[
       'email',
     ],
